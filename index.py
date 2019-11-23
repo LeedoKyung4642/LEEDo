5e4a81f9f46569f4bf2ea0fa7d923342bf60374b
LeedoKyung4642 <ike155@naver.com>
2019-11-24 06:02:28 +0900

    Update index.py

--- index.py
+++ index.py
@@ -385,9 +385,9 @@ if __name__ == '__main__':
 
     # -------- Main Loop Start --------
     while True:
-        key=cv2.waitkey(1)  #key==ord("a")
-        if key==27:
-            break
+        #key=cv2.waitkey(1)  #key==ord("a")
+        #if key==27:
+        #    break
 
          #grab the current frame
         (grabbed, frame) = camera.read()
